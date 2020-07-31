436cd354a0cc46f16dcefd0e0aafff36dfce6520
jonastrindade <jonastrinda@gmail.com>
2020-07-31 18:34:04 -0300

    devise, tabela user, bootstrap na login, signup e forgot

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'extrato#index'
   get 'extrato/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
