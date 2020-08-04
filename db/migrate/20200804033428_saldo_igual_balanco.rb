4ffeaafed87d7e308fe6fce735b04d7bf2d2d5b8
jonastrindade <jonastrinda@gmail.com>
2020-08-04 01:40:53 -0300

    teste banco 3 arg

--- db/migrate/20200804033428_saldo_igual_balanco.rb
+++ db/migrate/20200804033428_saldo_igual_balanco.rb
@@ -1,12 +1,12 @@
 class SaldoIgualBalanco < ActiveRecord::Migration[5.2]
   def change
     create_table :saldos do |t|
-      t.decimal :saldo, precision: 8, scale: 2
+      t.decimal :saldo, precision: 2
       t.references :user
       t.timestamps
     end
-
-    hange_column :saldos, :saldo, :decimal, precision: 8, scale: 2
+    
+    change_column :saldos, :saldo, :decimal, precision: 8, scale: 2
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
 
     User.all.each do |user|
