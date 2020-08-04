f297ff91381c1024a9aeafff90c0cb7066f57691
jonastrindade <jonastrinda@gmail.com>
2020-08-04 01:29:12 -0300

    teste banco

--- db/migrate/20200804005905_banco_med_cap2.rb
+++ db/migrate/20200804005905_banco_med_cap2.rb
@@ -1,5 +1,5 @@
 class BancoMedCap2 < ActiveRecord::Migration[5.2]
-  def down
+  def change
     create_table :saldos do |t|
       t.decimal :saldo, precision: 2
       t.references :user
@@ -9,6 +9,9 @@ class BancoMedCap2 < ActiveRecord::Migration[5.2]
     User.all.each do |u|
       Saldo.create!(user_id: u.id, saldo: u.saldo)
     end
+
+    remove_column :extratos, :saldo_extrato
+    remove_column :users, :saldo
 
     change_column :extratos, :taxa, :decimal, precision: 2
     change_column :extratos, :valor, :decimal
